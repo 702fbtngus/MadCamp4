45d2da5f73c2091e3dff17dff1e7b832a79ad099
hyeonji <hji0104@kaist.ac.kr>
2021-07-28 23:57:17 +0900

    merge fix

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import styled from 'styled-components';
-import Login from './components/Login';
-
-const Container = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background:
-  /* linear-gradient(
-      to bottom,
-      rgba(20, 20, 20, 0.1) 10%,
-      rgba(20, 20, 20, 0.7) 70%,
-      rgba(20, 20, 20, 1)
-    ), */
-	url("https://i.imgur.com/gxVnhA8.jpg");
-  background-size: cover;
-`;
-
-
+import "./App.css";
+import styled from "styled-components";
+import Login from "./components/Login";
+import React from "react";
 
 function App() {
-	return (
-		<Container>
-			<div className="App">
-				<Login />
-			</div>
-		</Container>
-	);
+  return <Login />;
 }
 
 export default App;
